72312cfb6531c5a4ec21beb9c07adfa76095c1d0
aderusmana <aderusmana777@gmail.com>
2025-01-31 10:12:23 +0700

    add simple page

---
+++ resources/views/page/mpdr/edit-mpdr.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    @section('title')
+        Edit MPDR
+    @endsection
+
+    @push('css')
+    @endpush
+
+
+    <div class="card bg-info-subtle shadow-none position-relative overflow-hidden mb-4">
+        <div class="card-body px-4 py-3">
+            <div class="row align-items-center">
+                <div class="col-9">
+                    <h4 class="fw-semibold mb-8">Edit MPDR</h4>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item">
+                                <a class="text-muted text-decoration-none" href="{{ route('dashboard') }}">Home</a>
+                            </li>
+                            <li class="breadcrumb-item" aria-current="page">Edit MPDR</li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="col-3">
+                    <div class="text-center mb-n5">
+                        <img src="{{ asset('assets') }}/images/breadcrumb/ChatBc.png" alt="modernize-img"
+                            class="img-fluid mb-n4">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <button onclick="history.back()" class="btn btn-sm btn-primary flex-end">Back</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+           <div class="table-responsive">
+
+           </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/page/pre-mpdr/edit-prempdr.blade.php
+++ resources/views/page/pre-mpdr/edit-prempdr.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     @section('title')
-        Create MPDR
+        Edit PREMPDR
     @endsection
 
     @push('css')
@@ -11,13 +11,13 @@
         <div class="card-body px-4 py-3">
             <div class="row align-items-center">
                 <div class="col-9">
-                    <h4 class="fw-semibold mb-8">Create MPDR</h4>
+                    <h4 class="fw-semibold mb-8">Edit PREMPDR</h4>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item">
                                 <a class="text-muted text-decoration-none" href="{{ route('dashboard') }}">Home</a>
                             </li>
-                            <li class="breadcrumb-item" aria-current="page">Create MPDR</li>
+                            <li class="breadcrumb-item" aria-current="page">Edit PREMPDR</li>
                         </ol>
                     </nav>
                 </div>
@@ -37,7 +37,7 @@
     <div class="card">
         <div class="card-body">
            <div class="table-responsive">
-           
+
            </div>
         </div>
     </div>
